a09a066a7dacfae498bbbed1dbdfd5bad30a7b9c
James Munns <james@onevariable.com>
2022-04-23 00:47:40 +0200

    Add char board (and probably some other stuff)

---
+++ boards/char/panelize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PYTHONPATH=/usr/lib/python3.9/site-packages/
+
+kikit panelize grid \
+    --space 3 \
+    --gridsize 1 2 \
+    --tabwidth 3 \
+    --tabheight 3 \
+    --htabs 2 \
+    --vtabs 1 \
+    --mousebites 0.5 1 -0.25 \
+    --radius 1 \
+    char.kicad_pcb \
+    char-panel.kicad_pcb \
+    --tolerance 1000
+
+
+D1-1 D2-1 D3-1 D4-1 D5-1 D6-1 D7-1 D8-1 D9-1 D10-1 D11-1 D12-1 D13-1 D14-1 D15-1 D16-1 D17-1 D18-1 D19-1 D20-1 D21-1 D22-1 D23-1 D24-1 D25-1 D26-1 D27-1 D28-1 D29-1 D30-1 D31-1 D32-1 D33-1 D34-1 D35-1 D36-1 D37-1 D38-1 D39-1 D40-1 D41-1 D42-1 D43-1 D44-1 D45-1 D46-1 D47-1 D48-1 D1-2 D2-2 D3-2 D4-2 D5-2 D6-2 D7-2 D8-2 D9-2 D10-2 D11-2 D12-2 D13-2 D14-2 D15-2 D16-2 D17-2 D18-2 D19-2 D20-2 D21-2 D22-2 D23-2 D24-2 D25-2 D26-2 D27-2 D28-2 D29-2 D30-2 D31-2 D32-2 D33-2 D34-2 D35-2 D36-2 D37-2 D38-2 D39-2 D40-2 D41-2 D42-2 D43-2 D44-2 D45-2 D46-2 D47-2 D48-2
